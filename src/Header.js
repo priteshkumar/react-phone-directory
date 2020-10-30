36acca1733638e881e58c86d8e501ffb26fef2a9
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-30 18:49:39 +0530

    add external header css

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import './Header.css';
+
 //import { Component } from 'react';
 
 //function components are stateless
 const Header = function () {
     return (
-        <div className="header" style={{ textAlign: 'center', padding: 20, backgroundColor: 'black', color:'white',textTransform: "uppercase" }}>
+        <div className="header">
             Phone Directory
         </div>
     );
 }
+
+/*
+inline style demo
+*
+const Header = function () {
+    return (
+        <div className="header" style={{ textAlign: 'center', padding: 20, backgroundColor: 'black', color: 'white', textTransform: "uppercase" }}>
+            Phone Directory
+        </div>
+    );
+}*/
+
 
 //class component example 
 //class components are used when state needs to be maintained
